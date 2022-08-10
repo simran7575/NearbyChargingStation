8dc1c60baf5819b2219770ade9afbf519b11dcfd
simran7575 <simranbedi7575@gmail.com>
2022-08-10 23:06:35 +0530

    Checking headers

--- middlewares/user.js
+++ middlewares/user.js
@@ -4,9 +4,9 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.isLoggedIn = BigPromise(async (req, res, next) => {
-  console.log("Line 7", req.header);
-  console.log("Line 7", req.header("Authorization"));
-  const token = req.header("Authorization").replace("Bearer ", "");
+  console.log("Line 7", req.headers);
+  console.log("Line 7", req.headers("Authorization"));
+  const token = req.headers("Authorization").replace("Bearer ", "");
   if (!token) {
     return res
       .status(200)
