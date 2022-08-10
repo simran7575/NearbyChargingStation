0103958bac72e18d95344e540b1f92b9edbde657
simran7575 <simranbedi7575@gmail.com>
2022-08-10 21:38:36 +0530

    fixed islogged in  error

--- middlewares/user.js
+++ middlewares/user.js
@@ -4,12 +4,11 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.isLoggedIn = BigPromise(async (req, res, next) => {
-  const token =
-    req.body.token || req.header("Authorization").replace("Bearer ", "");
+  const token = req.header("Authorization").replace("Bearer ", "");
   if (!token) {
-    res
-      .status(501)
-      .send(CustomError("Please log in to find more information", 501));
+    return res
+      .status(200)
+      .json(CustomError("Please log in to find more information", 501));
   }
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   req.user = await User.findById(decoded.id);
