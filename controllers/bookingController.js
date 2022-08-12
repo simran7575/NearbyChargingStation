a9fe5ae14c0e9f1e8d8d84516439fe0fe99dd6cb
simran7575 <simranbedi7575@gmail.com>
2022-08-13 00:43:24 +0530

    Testing query params

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -88,7 +88,8 @@ exports.cancelBooking = BigPromise(async (req, res, next) => {
   });
 });
 exports.getOneBooking = BigPromise(async (req, res, next) => {
-  console.log(req.query.id);
+  console.log("Line 91", req.query);
+  console.log("Line 92", req.params);
   const booking = await Booking.findById(req.query.id);
   if (!booking) {
     return res.status(200).json(CustomError("No Bookings found", 404));
